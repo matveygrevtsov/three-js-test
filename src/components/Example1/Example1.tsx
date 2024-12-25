462c10c49d130d4ca680a05a63381258561ea456
matveygrevtsov <matveygrevcov@gmail.com>
2024-12-25 15:18:19 +0300

    Updates

--- src/components/Example1/Example1.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from "react";
-import { useScene } from "./useScene";
-
-export const Example1: FC = () => {
-  const ref = useScene();
-  return <div ref={ref} />;
-};
--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export type TGetObjectValues<T extends object> = T[keyof T];
-
-export interface IRoute {
-  title: string;
-  pathname: string;
-  element: ReactNode;
-}
